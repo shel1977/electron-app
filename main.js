488e4c5ae78eec806a64f1325591ec4c881da778
shel1977 <st-shel@yandex.ru>
2020-05-28 19:57:09 +0300

    use axios

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path');
 const url = require('url');
 const {app, BrowserWindow} = require('electron');
 const {ipcMain} = require('electron');
-const request = require('request');
+
 
 
 let win;
@@ -32,57 +32,10 @@ const createWindow = () => {
     })
 };
 
-ipcMain.on('getData', function (event) {
-    console.log('ipc work');
-    getData();
-    event.sender.send('replyFromGet', responseData)
-
-
-});
-ipcMain.on('postData', function (event, inputDataArea) {
-    console.log('ipc post work');
-    postData(inputDataArea);
-    event.reply('replyFromPost', responseData)
-});
-
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {   //close app on macOs
     app.quit();
 });
 
-let responseData;
-
-const getData = () => {
-    request(
-        'http://localhost:8888/data',
-        {json: true},
-        (err, res, body) => {
-            if (err) {
-                return console.log(err);
-            }
-            responseData = body;
-            console.log(body);
-
-        }
-    )
-};
-
-const postData = (data) => {
-    let createDataObject = {
-        data: data
-    };
-    request.post({
-            url: 'http://localhost:8888/post',
-            json: true,
-            body: createDataObject
-        },
-        (err, res, body) => {
-            if (err) {
-                return console.log(err);
-            }
-            responseData = body
-        }
-    )
-};
 
--- render.js
+++ render.js
@@ -1,9 +1,22 @@
+const axios = require('axios');
 const {ipcRenderer} = require('electron');
+const request = require('request');
+
 
 const getDataButton = document.getElementById('getDataButton');
 const postDataButton = document.getElementById('postDataButton');
 
 getDataButton.addEventListener('click', function () {
+    getData()
+});
+
+postDataButton.addEventListener('click', function () {
+    const inputDataArea = document.getElementById('inputDataArea').value;
+    postData(inputDataArea);
+    document.getElementById('inputDataArea').value = ''
+});
+
+/*getDataButton.addEventListener('click', function () {
     ipcRenderer.send('getData');
     ipcRenderer.once('replyFromGet', (event, value) => {
         console.log('answer ' + value.data);
@@ -18,9 +31,27 @@ postDataButton.addEventListener('click', function () {
         console.log('answer ' + value.data);
         showFunc(value.data)
     });
-});
+});*/
 
 //show answer
 const showFunc = (value) => {
     document.getElementById('showResponseData').innerHTML = value;
+};
+
+
+const getData = () => {
+    axios.get('http://localhost:8888/data')
+        .then((response) => {
+            console.log(response.data.data);
+
+            showFunc(response.data.data)
+        });
+};
+
+const postData = (text) => {
+    axios.post('http://localhost:8888/post',
+        {data: text})
+        .then((response) => {
+            showFunc(response.data.info)
+        });
 };
